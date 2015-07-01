6dfad890610e8d7dfa118cdc79a73823d89d0b03
Arthur M. Segura <art@leadcommerce.com>
2015-07-01 13:42:05 -0700

    change CURLOPT_VERBOSE  to 0; remove extra $http_status variable

--- connection.php
+++ connection.php
@@ -277,7 +277,7 @@ class connection
 		curl_setopt($curl, CURLOPT_URL, $url);
 		curl_setopt($curl, CURLOPT_HTTPHEADER, $this->_headers);
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($curl, CURLOPT_VERBOSE, 1);
+		curl_setopt($curl, CURLOPT_VERBOSE, 0);
 		curl_setopt($curl, CURLOPT_HEADER, 1);
 		curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
 		curl_setopt($curl, CURLOPT_HTTPGET, 1);
@@ -286,7 +286,6 @@ class connection
 		
 		$response = curl_exec($curl);
 
-		$http_status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 		$header_size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
 		$headers = substr($response, 0, $header_size);
 		$body = substr($response, $header_size);
